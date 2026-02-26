708498a3891e3e91d9f12f878ad221645cfc1dcc
nweze kingsley <nwezek04@gmail.com>
2026-02-26 15:13:08 +0100

    update every

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function toggleMenu() {
-  const menu = document.getElementById("navLinks");
-  menu.classList.toggle("show");
+const navMenu = document.getElementById("navLinks");
+const openIcon = document.getElementById("openIcon");
+const closeIcon = document.getElementById("closeIcon");
+
+function openMenu() {
+  navMenu.style.display = "inline-block";   
+  openIcon.style.display = "none"; 
+  closeIcon.style.display = "inline-block";
+  openIcon.style.float = "right";
+  navMenu.style.float = "right";
+  navMenu.style.position = "fixed";
+  navMenu.style.left = "0px";
+  navMenu.style.marginTop = "42px";
   
+}
+
+function closeMenu() {
+  navMenu.style.display = "none";   
+  openIcon.style.display = "block"; 
+  closeIcon.style.display = "none"; 
+ 
+ 
 }
